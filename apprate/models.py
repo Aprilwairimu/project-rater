5080dea956cac3936de6f3d7a72d925ee2e5053d
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-06-10 22:36:29 +0300

    installed pillow

--- apprate/models.py
+++ apprate/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200,null=True,blank=True)
+    image = models.ImageField ( upload_to='pictures/',null=True, blank=True)
+    description = models.CharField(max_length=300,null=True, blank=True)
+
+class Profile(models.Model):
+    prof_pic =  models.ImageField (null=False, blank=False)
+    bio = models.TextField(max_length=100, null=True, blank=True)
+    # posted_projects
+    # contact
--- apprate/views.py
+++ apprate/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse,Http404
 from .forms import RegisterForm,LoginForm
+from urllib import request
 from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
@@ -8,9 +9,37 @@ from django.contrib.auth import authenticate,login,logout
 
 def home(request):
     return render(request,'home.html')
+    
+
+def logout(request):
+    logout(request)
+    return redirect('home')
+
 
 def register(request):
-    return render(request,'register/register.html',)
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request,'register/register.html', {"form":form})
 
 def login(request):
-    return render(request,'register/login.html')
+    form=LoginForm()
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['email']
+            password=form.cleaned_data['password']
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                return HttpResponse('Such a user does not exist')
+        else:
+            return HttpResponse("Form is not Valid")
+    return render(request,'register/login.html',{'form':form})

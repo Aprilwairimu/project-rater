889d4308ce9ae4ddc95dd62339de7acfa01845ac
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-06-11 12:48:09 +0300

    registered class models

---
+++ apprate/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Profile,Post,Rate
+# Register your models here.
+
+admin.site.register(models.Profile)
+admin.site.register(models.Post)
+admin.site.register(models.Rate)
